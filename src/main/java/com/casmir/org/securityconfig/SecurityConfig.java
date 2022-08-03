21aaf773ca0c1afb0bd71d99488ae33725aa442a
ugwuchukwuogbonnaya <stcasmir4excel@gmail.com>
2022-08-03 11:12:38 +0100

    Inmemory Authentication Completed

--- src/main/java/com/casmir/org/securityconfig/SecurityConfig.java
+++ src/main/java/com/casmir/org/securityconfig/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.casmir.org.securityconfig;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 
 @Configuration
@@ -17,8 +19,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		.antMatchers("/home").permitAll()
 		.and()
 		.httpBasic();
+			
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+		.inMemoryAuthentication()
+		.withUser("james").password("james123").authorities("USER")
+		.and()
+		.withUser("john").password("john123").authorities("ADMIN")
+		.and()
+		.passwordEncoder(NoOpPasswordEncoder.getInstance());
 		
-		
+	
 	}
 
 }
